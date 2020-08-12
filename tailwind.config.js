6a1bf1367b23b48a7c4a7a86485c31c09e8046ee
Ricardo Amaral <master@ricardoamaral.net>
2020-08-12 22:48:43 +0100

    Add new `min-w-screen-md` utility to Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
     purge: ['./src/**/*.tsx', './src/index.html'],
-    theme: {},
+    theme: {
+        minWidth: {
+            '0': '0',
+            'screen-md': '768px',
+            full: '100%',
+        },
+    },
     variants: {},
     plugins: [],
 };
