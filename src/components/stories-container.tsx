d3fdca554cef4377a339909820b529b1dcdab6b6
Ricardo Amaral <master@ricardoamaral.net>
2020-08-13 00:37:03 +0100

    Implement infinite scroll mechanism

--- src/components/stories-container.tsx
+++ src/components/stories-container.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 
-import { useHackerNews } from '../hooks';
+import { useHackerNews, useInfiniteScroll } from '../hooks';
 
 import { StoryItem } from './story-item';
 
+const MAX_STORY_ITEMS = 500;
+const STORY_ITEM_INCREMENT = 15;
+
 function StoriesContainer(): JSX.Element {
+    const [infiniteScrollRef, storyCount] = useInfiniteScroll(
+        STORY_ITEM_INCREMENT,
+        MAX_STORY_ITEMS
+    );
+
     const storyIds = useHackerNews('newstories');
 
     return (
         <>
             <main className="flex flex-row flex-wrap justify-center space-y-5">
                 {storyIds.isLoaded &&
-                    storyIds.data.slice(0, 15).map((id: number) => <StoryItem key={id} id={id} />)}
-                <div />
+                    storyIds.data.slice(0, storyCount).map((id: number, index: number) => {
+                        const secondToLastRef =
+                            storyCount < MAX_STORY_ITEMS && index + 2 === storyCount
+                                ? infiniteScrollRef
+                                : undefined;
+
+                        return (
+                            <div className="w-full" key={id} ref={secondToLastRef}>
+                                <StoryItem id={id} />
+                            </div>
+                        );
+                    })}
             </main>
         </>
     );
