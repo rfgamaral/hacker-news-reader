665913060f50e8a12731a67277dce0ba76f2a71a
Ricardo Amaral <master@ricardoamaral.net>
2020-08-12 22:50:29 +0100

    Update base `Application` component

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { StoriesContainer } from './stories-container';
+
 function Application(): JSX.Element {
     return (
-        <>
-            <header data-testid="header">
-                <h1 data-testid="title">Hacker News Reader</h1>
+        <div className="min-w-screen-md max-w-screen-lg mx-auto px-5 mb-12">
+            <header className="text-center my-5">
+                <h1 className="font-semibold text-6xl text-gray-800">Hacker News Reader</h1>
             </header>
-        </>
+            <StoriesContainer />
+        </div>
     );
 }
 
